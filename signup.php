6be139d29ea1a82b8351af09946537db0cd8ea95
jargyi <admin@sinphyukyun.com>
2017-08-11 15:44:53 +0630

    Add register form

--- signup.php
+++ signup.php
@@ -79,151 +79,140 @@ require_once 'template/nav.php';
             </div>
 
 
-            <div class="wrap-input form-group">
-                <label><span id="BodyPlaceHolder_LabelEmail">Email</span><span
-                            class="star"> *</span></label>
-                <i class="fa fa-envelope"></i>
-                <input name="ctl00$BodyPlaceHolder$txtEmail" type="text" maxlength="50"
-                       id="BodyPlaceHolder_txtEmail" class="form-control" placeholder="E-mail"/>
+            <form action="function/login.php" method="post">
+                <div class="wrap-input form-group">
+                    <label><span id="BodyPlaceHolder_LabelEmail">Email
+                    </span><span class="star"> *</span>
+                    </label>
+                    <i class="fa fa-envelope"></i>
+                    <input name="email" type="text" maxlength="50" class="form-control" placeholder="E-mail"/>
+                </div>
+                <div class="wrap-input form-group">
+                    <label>
+                        <span id="BodyPlaceHolder_lblPassword">Password</span>
+                        <span class="star"> *</span></label>
+                    <i class="fa fa-lock"></i>
+                    <input name="password" type="password" maxlength="18" id="BodyPlaceHolder_txtPassword" class="form-control" placeholder="Password" minlength="4"/>
 
+                </div>
+                <div class="wrap-input form-group">
+                    <label>
+                        <span id="BodyPlaceHolder_lblFirstName">First Name</span>
+                        <span class="star"> *</span>
+                    </label>
+                    <i class="fa fa-user"></i>
+                    <input name="first_name" type="text" maxlength="50" id="BodyPlaceHolder_txtFirstName" class="form-control" placeholder="Name"/>
 
-            </div>
-            <div class="wrap-input form-group">
-                <label><span id="BodyPlaceHolder_lblPassword">Password</span><span
-                            class="star"> *</span></label>
-                <i class="fa fa-lock"></i>
+                </div>
+                <div class="wrap-input form-group">
+                    <label>
+                        <span id="BodyPlaceHolder_lblLastName">Last Name</span>
+                        <span class="star"> *</span></label>
+                    <i class="fa fa-user"></i>
 
-                <input name="ctl00$BodyPlaceHolder$txtPassword" type="password" maxlength="18"
-                       id="BodyPlaceHolder_txtPassword" class="form-control" placeholder="Password"
-                       minlength="4"/>
+                    <input name="last_name" type="text" maxlength="50" id="BodyPlaceHolder_txtLastName" class="form-control" placeholder="Name"/>
+                </div>
+                <div class="wrap-input form-group">
+                    <label>
+                        <span id="BodyPlaceHolder_LabelPhoneNumber">Phone Number</span>
+                        <span class="star"> *</span></label>
+                    <i class="fa fa-phone"></i>
 
-            </div>
-            <div class="wrap-input form-group">
-                <label><span id="BodyPlaceHolder_lblFirstName">First Name</span><span class="star"> *</span></label>
-                <i class="fa fa-user"></i>
+                    <input name="phone" type="text" maxlength="20" id="BodyPlaceHolder_txtPhoneNumber" class="sign-up-input" placeholder="+95 1 502 077"/>
+                </div>
+                <div class="wrap-input form-group">
+                    <label>
+                        <span id="BodyPlaceHolder_lblJobTitle">What are you looking for</span><span
+                                class="star"> *</span></label>
+                    <i class="fa fa-user"></i>
+                    <input name="looking_for" type="text" maxlength="50" id="BodyPlaceHolder_txtJobTitle" class="sign-up-input" placeholder="Job Title"/>
 
-                <input name="ctl00$BodyPlaceHolder$txtFirstName" type="text" maxlength="50"
-                       id="BodyPlaceHolder_txtFirstName" class="form-control" placeholder="Name"/>
+                </div>
+                <div class="wrap-input form-group">
+                    <label>
+                        <span id="BodyPlaceHolder_lblJobFunction">Your Primary Job Function</span>
+                        <span class="star"> *</span></label>
+                    <select name="ctl00$BodyPlaceHolder$ddlJobFunction" id="BodyPlaceHolder_ddlJobFunction" class="sign-up-select">
+                        <option value="0">Choose Job Function</option>
+                        <option value="1">Accounting/Finance</option>
+                        <option value="2">Administrative</option>
+                        <option value="4">Consulting</option>
+                        <option value="6">Customer Service/Support</option>
+                        <option value="7">Design/Architecture</option>
+                        <option value="8">Driver/Security/Cleaning</option>
+                        <option value="9">Education/Teaching/Childcare</option>
+                        <option value="10">Engineering/Technical</option>
+                        <option value="12">Fitness/Wellbeing</option>
+                        <option value="13">Food and Beverage</option>
+                        <option value="14">Hair/Nails and Beauty</option>
+                        <option value="16">HR/Training and Recruitment</option>
+                        <option value="15">Hospitality/Tourism</option>
+                        <option value="17">IT Hardware/Software</option>
+                        <option value="18">Legal</option>
+                        <option value="19">Logistics/Distribution</option>
+                        <option value="20">Management</option>
+                        <option value="21">Manufacturing/Warehouse</option>
+                        <option value="22">Marketing/Media/Creative</option>
+                        <option value="23">Medical/Doctor/Nursing</option>
+                        <option value="25">PR/Communications</option>
+                        <option value="38">Procurement/Sourcing</option>
+                        <option value="24">Project Management</option>
+                        <option value="26">Quality Assurance</option>
+                        <option value="27">Research and Development</option>
+                        <option value="30">Sales/Business Development</option>
+                        <option value="31">Science/Technology</option>
+                        <option value="32">Skilled Trades / Construction</option>
+                        <option value="33">Strategy/Planning</option>
+                        <option value="34">Translation</option>
+                        <option value="35">Voluntary work</option>
+                        <option value="36">Writing/Editing</option>
+                        <option value="37">Others</option>
+                    </select>
 
-            </div>
-            <div class="wrap-input form-group">
-                <label><span id="BodyPlaceHolder_lblLastName">Last Name</span><span
-                            class="star"> *</span></label>
-                <i class="fa fa-user"></i>
+                </div>
+                <div class="wrap-input form-group">
+                    <label>
+                        <span id="BodyPlaceHolder_lblExperienceLevel">Your Experience Level</span>
+                        <span class="star"> *</span>
+                    </label>
+                    <select name="experience" id="BodyPlaceHolder_ddlExperienceLevel" class="sign-up-select">
+                        <option value="0">Choose Experience Level</option>
+                        <option value="1">Entry Level</option>
+                        <option value="2">Experienced Non-Manager</option>
+                        <option value="3">Manager</option>
+                        <option value="4">Director and Above</option>
+                    </select>
 
-                <input name="ctl00$BodyPlaceHolder$txtLastName" type="text" maxlength="50"
-                       id="BodyPlaceHolder_txtLastName" class="form-control" placeholder="Name"/>
+                </div>
+                <div class="wrap-input form-group">
+                    <label>
+                        <span id="BodyPlaceHolder_lblLocation">Your Current Location</span>
+                        <span class="star"> *</span></label>
+                    <select name="locaton" id="BodyPlaceHolder_ddlLocation" class="sign-up-select">
+                        <option value="0">Choose Location</option>
+                        <option selected="selected" value="1">Yangon</option>
+                        <option value="2">Mandalay</option>
+                        <option value="3">NayPyiTaw</option>
+                        <option value="9">Ayeyawady Division</option>
+                        <option value="5">Bago Division</option>
+                        <option value="14">Chin State</option>
+                        <option value="10">Kachin State</option>
+                        <option value="15">Kayah State</option>
+                        <option value="13">Kayin State</option>
+                        <option value="7">Magway Division</option>
+                        <option value="8">Mon State</option>
+                        <option value="11">Rakhine State</option>
+                        <option value="6">Sagaing Division</option>
+                        <option value="4">Shan State</option>
+                        <option value="12">Taninthayi Division</option>
+                        <option value="16">International</option>
+                    </select>
 
-            </div>
-            <div class="wrap-input form-group">
-                <label><span id="BodyPlaceHolder_LabelPhoneNumber">Phone Number</span><span
-                            class="star"> *</span></label>
-                <i class="fa fa-phone"></i>
-
-
-                <input name="ctl00$BodyPlaceHolder$txtPhoneNumber" type="text" maxlength="20"
-                       id="BodyPlaceHolder_txtPhoneNumber" class="sign-up-input"
-                       placeholder="+95 1 502 077"/>
-            </div>
-            <div class="wrap-input form-group">
-                <label><span id="BodyPlaceHolder_lblJobTitle">What are you looking for</span><span
-                            class="star"> *</span></label>
-                <i class="fa fa-user"></i>
-                <input name="ctl00$BodyPlaceHolder$txtJobTitle" type="text" maxlength="50"
-                       id="BodyPlaceHolder_txtJobTitle" class="sign-up-input" placeholder="Job Title"/>
-
-            </div>
-            <div class="wrap-input form-group">
-                <label><span id="BodyPlaceHolder_lblJobFunction">Your Primary Job Function</span><span
-                            class="star"> *</span></label>
-                <select name="ctl00$BodyPlaceHolder$ddlJobFunction" id="BodyPlaceHolder_ddlJobFunction"
-                        class="sign-up-select">
-                    <option value="0">Choose Job Function</option>
-                    <option value="1">Accounting/Finance</option>
-                    <option value="2">Administrative</option>
-                    <option value="4">Consulting</option>
-                    <option value="6">Customer Service/Support</option>
-                    <option value="7">Design/Architecture</option>
-                    <option value="8">Driver/Security/Cleaning</option>
-                    <option value="9">Education/Teaching/Childcare</option>
-                    <option value="10">Engineering/Technical</option>
-                    <option value="12">Fitness/Wellbeing</option>
-                    <option value="13">Food and Beverage</option>
-                    <option value="14">Hair/Nails and Beauty</option>
-                    <option value="16">HR/Training and Recruitment</option>
-                    <option value="15">Hospitality/Tourism</option>
-                    <option value="17">IT Hardware/Software</option>
-                    <option value="18">Legal</option>
-                    <option value="19">Logistics/Distribution</option>
-                    <option value="20">Management</option>
-                    <option value="21">Manufacturing/Warehouse</option>
-                    <option value="22">Marketing/Media/Creative</option>
-                    <option value="23">Medical/Doctor/Nursing</option>
-                    <option value="25">PR/Communications</option>
-                    <option value="38">Procurement/Sourcing</option>
-                    <option value="24">Project Management</option>
-                    <option value="26">Quality Assurance</option>
-                    <option value="27">Research and Development</option>
-                    <option value="30">Sales/Business Development</option>
-                    <option value="31">Science/Technology</option>
-                    <option value="32">Skilled Trades / Construction</option>
-                    <option value="33">Strategy/Planning</option>
-                    <option value="34">Translation</option>
-                    <option value="35">Voluntary work</option>
-                    <option value="36">Writing/Editing</option>
-                    <option value="37">Others</option>
-
-                </select>
-
-            </div>
-            <div class="wrap-input form-group">
-                <label><span id="BodyPlaceHolder_lblExperienceLevel">Your Experience Level</span><span
-                            class="star"> *</span></label>
-                <select name="ctl00$BodyPlaceHolder$ddlExperienceLevel"
-                        id="BodyPlaceHolder_ddlExperienceLevel" class="sign-up-select">
-                    <option value="0">Choose Experience Level</option>
-                    <option value="1">Entry Level</option>
-                    <option value="2">Experienced Non-Manager</option>
-                    <option value="3">Manager</option>
-                    <option value="4">Director and Above</option>
-
-                </select>
-
-            </div>
-            <div class="wrap-input form-group">
-                <label><span id="BodyPlaceHolder_lblLocation">Your Current Location</span><span
-                            class="star"> *</span></label>
-                <select name="ctl00$BodyPlaceHolder$ddlLocation" id="BodyPlaceHolder_ddlLocation"
-                        class="sign-up-select">
-                    <option value="0">Choose Location</option>
-                    <option selected="selected" value="1">Yangon</option>
-                    <option value="2">Mandalay</option>
-                    <option value="3">NayPyiTaw</option>
-                    <option value="9">Ayeyawady Division</option>
-                    <option value="5">Bago Division</option>
-                    <option value="14">Chin State</option>
-                    <option value="10">Kachin State</option>
-                    <option value="15">Kayah State</option>
-                    <option value="13">Kayin State</option>
-                    <option value="7">Magway Division</option>
-                    <option value="8">Mon State</option>
-                    <option value="11">Rakhine State</option>
-                    <option value="6">Sagaing Division</option>
-                    <option value="4">Shan State</option>
-                    <option value="12">Taninthayi Division</option>
-                    <option value="16">International</option>
-
-                </select>
-
-            </div>
-
-
-            <div class="wrap-input">
-
-                <input type="submit" name="ctl00$BodyPlaceHolder$btnSave" value="Register"
-                       onclick="javascript:WebForm_DoPostBackWithOptions(new WebForm_PostBackOptions(&quot;ctl00$BodyPlaceHolder$btnSave&quot;, &quot;&quot;, true, &quot;save&quot;, &quot;&quot;, false, false))"
-                       id="BodyPlaceHolder_btnSave" class="login-button" style="width: 100%"/>
-            </div>
+                </div>
+                <div class="wrap-input">
+                    <input type="submit" name="Add Data" value="Register" id="BodyPlaceHolder_btnSave" class="login-button" style="width: 100%"/>
+                </div>
+            </form>
 
 
             <span>Already have an account?</span>
